318505cc43636af08e3d89d5a69a621ba8ebd253
gpnabugoomu <gpnabugoomu@gmail.com>
2023-08-28 07:28:41 -0700

    100-safe

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format().
+
+    If ValueError message is caught, then a corresponding
+    message should be printed to standard error.
+
+    Args:
+        value (int): integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
