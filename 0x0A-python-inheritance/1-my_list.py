22f80224da44a25a5e0853c34bae69b7ac107c0e
gpnabugoomu <gpnabugoomu@gmail.com>
2023-09-11 08:54:38 -0700

    new

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
-"""This module inherits from the list class"""
+"""
+This module inherits from the list class
+"""
 
 
 class MyList(list):
-    """A class that inherits from list"""
+    """A class that inherits from list
+    """
     def print_sorted(self):
-        """prints a sorted list"""
-        print(sorted(self))
+        """prints a sorted list in ascending order"""
+
+        sortedlist = sorted(self)
+        print(sortedlist)
