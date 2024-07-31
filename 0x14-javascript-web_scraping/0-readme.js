30f54c2f2caadc072570d99c1ecc774a7500e579
gpnabugoomu <gpnabugoomu@gmail.com>
2024-07-31 12:09:56 +0300

    new

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', (error, responce) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(responce);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
